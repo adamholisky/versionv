4aea3bd7959a71429cc1e123c38a36994cffd7a3
Adam Holisky <adam.holisky@gmail.com>
2023-04-19 18:44:28 -0700

    .

--- support/libtest/src/myshared.c
+++ support/libtest/src/myshared.c
@@ -3,5 +3,5 @@
 void my_lib_call( void );
 
 void my_lib_call( void ) {
-    printf( "Hello shared library, world!\n" );
+    printf( "This function and call were modified without an OS recompile; only with a library recompile that was then copied into the secondary drive.\n" );
 }
